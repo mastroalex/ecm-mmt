8ca5f0043c5bc05b136d1dab8c7d68704bd4d8ad
mastroalex <76497903+mastroalex@users.noreply.github.com>
2022-06-14 08:39:33 +0200

    code

--- code/fcm_mix.m
+++ code/fcm_mix.m
@@ -1,4 +1,5 @@
-function eps_mix=fcm_mix(fCM,eps_med,vol_frac)
-multiply=(1+2*vol_frac*fCM)./(1-vol_frac.*fCM);
-eps_mix=eps_med.*(multiply);
+function eps_mix=fcm_mix(fCM,epstilde_med,vol_frac)
+multiply=(1+2*vol_frac.*fCM)./(1-vol_frac.*fCM);
+eps_mix=epstilde_med.*(multiply);
 end
+
--- code/mixture.m
+++ code/mixture.m
@@ -1,4 +1,4 @@
 function epstilde=mixture(mem,int)
 chi=(mem.epstilde./mem.thickness)./(int.epstilde./int.radius);
-epstilde=int.epstilde.*(chi/(1+chi));
+epstilde=int.epstilde.*(chi)./(1+chi);
 end
